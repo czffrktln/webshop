b2fad385f62e75a6f21c0333da77de80f4cead58
KrisztinaBrczan <brczan.kriszti@gmail.com>
2025-01-30 15:13:24 +0100

    add cors

--- backend/index.ts
+++ backend/index.ts
@@ -1,30 +1,34 @@
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 
 import express, { Express, Request, Response } from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
-const mongourl = process.env.MONGOURL as string
-const port = process.env.PORT
+const mongourl = process.env.MONGOURL as string;
+const port = process.env.PORT;
 
-mongoose.connect(mongourl, {})
+mongoose
+  .connect(mongourl, {})
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
     app.listen(port, () => {
-      console.log('Server is running on port ' + port);
-    })
+      console.log("Server is running on port " + port);
+    });
   })
   .catch((error) => {
-    console.log('Error connecting to MongoDB', error);
-  })
+    console.log("Error connecting to MongoDB", error);
+  });
 
 app.get("/", function (req: Request, res: Response) {
-  res.send("Hello, World!");
+  console.log(req);
+
+  res.send({ text: "Hello, World!" });
 });
 
-
-export default app
+export default app;
